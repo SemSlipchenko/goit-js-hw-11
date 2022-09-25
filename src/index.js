d4636b6ab609e5f9c89bfd746c048bd92cb0e463
SemSlipchenko <semslipchenko@gmail.com>
2022-09-25 20:04:32 +0300

    adding fetch pixabay

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import axios from "axios";
-import debounce from "lodash.debounce";
-import Notiflix from "notiflix";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from "axios"
+import Notiflix from "notiflix"
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+const form = document.querySelector('#search-form')
+const axios = require('axios').default;
+const URL = 'https://pixabay.com/api/'
+
+form.addEventListener('submit', onFromSubmit)
+
+function onFromSubmit(event) { 
+    event.preventDefault()
+    const searchQuary = event.currentTarget.elements.searchQuery.value
+    async function fetchImage(){ 
+        try {
+        const response = await axios.get(`${URL}?key=30149024-203e7bcb772de078758336c7f&q=${searchQuary}&image_type=photo&orientation=horizontal&safesearch=true`)
+        console.log(response.data.hits)
+    } catch (error) {
+        console.error(error)
+        }
+    }
+    fetchImage()
+}
+
+
+
